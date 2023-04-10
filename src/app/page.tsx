a2d054927c0f1338f30c6a6793f8447825f373db
fallenprodigy <giorgi.kalatozishvili.1@btu.edu.ge>
2023-04-11 03:19:23 +0400

    feat:  svg logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import Header from "@/app/components/Header";
+import Hero from "@/app/components/Hero";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Giorgi Kalatozi</h1>
+    <main className="">
+      <Header />
+      <Hero />
     </main>
   );
 }
