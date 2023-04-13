71704a42f5b490859f4519d30908c5c636ea7413
fallenprodigy <giorgi.kalatozishvili.1@btu.edu.ge>
2023-04-14 01:37:42 +0400

    hero section

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -7,34 +7,35 @@ type Props = {};
 export default function Header({}: Props) {
   return (
     <div className="max-w-4xl mx-auto py-7 ">
-      <div className="flex justify-between ">
+      <div className="flex justify-between items-center ">
         {/* LOGO */}
         <div>
-          <h1 className="text-xl font-semibold">
+          <h1 className="text-3xl font-bold">
             {/* <Logo /> */}
-            Giorgi Kalatozi
+            GK
+            {/* Giorgi Kalatozi */}
           </h1>
         </div>
         {/* NAVBAR */}
         <nav className="">
           <ul className="flex gap-4">
             <li>
-              <Link className="hover:underline " href="#">
+              <Link className="hover:underline text-[16px] " href="#">
                 About
               </Link>
             </li>
             <li>
-              <Link className="hover:underline " href="#">
+              <Link className="hover:underline text-[16px] " href="#">
                 Projects
               </Link>
             </li>
             <li>
-              <Link className="hover:underline" href="#">
+              <Link className="hover:underline text-[16px]" href="#">
                 Skills
               </Link>
             </li>
             <li>
-              <Link className="hover:underline" href="#">
+              <Link className="hover:underline text-[16px]" href="#">
                 Contact
               </Link>
             </li>
@@ -42,7 +43,7 @@ export default function Header({}: Props) {
         </nav>
         {/* ICONS */}
         <div>
-          <BsMoonStars />
+          <BsMoonStars fontSize="20px" />
         </div>
       </div>
     </div>
--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,34 +1,32 @@
-import Image from "next/image";
-import React from "react";
-import Headshot from "./../../../public/headshot.png";
-
 type Props = {};
-{
-  /* <h3 className="text-3xl font-semibold text-gray-300">
-            Hi, My name is Giorgi Kalatozi.
-          </h3> */
-}
+
 export default function Hero({}: Props) {
   return (
-    <div className="max-w-4xl mx-auto mt-40 ">
+    <div className="max-w-4xl mx-auto mt-32 ">
       <div className="flex">
-        <div className="flex-1 justify-between ">
-          <div className="text-6xl font-bold ">Software Developer.</div>
+        <div className="flex-1 flex flex-col justify-between gap-4">
+          <h3 className=" text-[20px] text-gray-300">Hi, My name is</h3>
+          <div className="text-6xl font-bold ">Giorgi Kalatozi.</div>
           <p className="text-gray-400 font-bold text-5xl">
             I build things for the web.
           </p>
-        </div>
-        {/* <Image
-          className="object-cover"
-          width={100}
-          height={1000}
-          alt="headshot"
-          src={Headshot}
-        /> */}
-        {/* <p className="flex-1 ">Beautiful, fast and modern React UI library.</p> */}
-      </div>
 
-      <div></div>
+          <div>
+            <p className="text-gray-400">
+              I’m a full stack software developer specializing in building
+              beautiful, fast and modern
+            </p>
+            <p className="text-gray-400">
+              web applications. Currently, I am committed to becoming a better
+              developer with a
+            </p>
+            <p className="text-gray-400">
+              passion for technology and a desire to constantly learn and
+              improve.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
