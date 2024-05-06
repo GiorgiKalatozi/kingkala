a15592b13afcce73a2655cf10912772b504ebf97
GiorgiKalatozi <giorgikalatoz@gmail.com>
2024-05-07 00:23:31 +0400

    chore: add layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
-import { ThemeProvider } from "@/lib/providers";
 import "./globals.css";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Giorgi Kalatozi | Developer",
@@ -13,18 +15,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <ThemeProvider>
-        <body
-          className={
-            "dark:bg-gradient-to-r dark:from-black  dark:via-blue-950  dark:to-black dark:text-white bg-gradient-to-r from-orange-300 to-rose-300 text-black px-7"
-            // "bg-gradient-to-r from-slate-900 via-purple-900 to-slate-900"
-            // "bg-gradient-to-r from-orange-300 to-rose-300 text-black"
-            // "bg-gradient-to-r from-gray-100 to-gray-300"
-          }
-        >
-          {children}
-        </body>
-      </ThemeProvider>
+      <body
+        className={`${inter.className} bg-[#030014] overflow-y-scroll overflow-x-hidden`}
+      >
+        {children}
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,7 @@
-import Header from "@/components/Header";
-import Hero from "@/components/Hero";
-import { getQuotes } from "@/services/api";
-import { Quote } from "./types";
-import About from "@/components/About";
-import Skills from "@/components/Skills";
-import Experience from "@/components/Experience";
-import Projects from "@/components/Projects";
-import Footer from "@/components/Footer";
-
 export default async function Home() {
-  const quote: Quote = await getQuotes();
-
   return (
-    <>
-      <Header />
-      <Hero quote={quote} />
-      {/* <About />
-      <Skills />
-      <Experience />
-      <Projects />
-      <Footer /> */}
-    </>
+    <main className="h-full w-full">
+      <div className="flex flex-col gap-20"></div>
+    </main>
   );
 }
